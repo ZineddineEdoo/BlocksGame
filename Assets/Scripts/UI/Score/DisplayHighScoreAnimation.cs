a62b57bc68a855d7a9ed684473a6ca16404cbaa4
Zineddine Edoo <zineddine.edoo@outlook.com>
2019-10-31 19:18:15 +0800

    Added New High Score Text Animation

---
+++ Assets/Scripts/UI/Score/DisplayHighScoreAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// Requires Animator Component in Parent Hierarchy
+/// </summary>
+public class DisplayHighScoreAnimation : MonoBehaviour
+{
+	void Awake()
+	{
+		if (Globals.Score > Globals.PreviousHighScore)
+			GetComponentInParent<Animator>().SetBool("IsNewHighScore", true);
+	}
+}
