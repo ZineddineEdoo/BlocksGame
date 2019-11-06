8f8c31b75ea5475e849f597e38b7268048eeec2a
Zineddine Edoo <zineddine.edoo@outlook.com>
2019-11-06 15:40:39 +0800

    Modified Difficulty

--- Assets/Scripts/Generator/ChangeZoneDifficulty.cs
+++ Assets/Scripts/Generator/ChangeZoneDifficulty.cs
@@ -25,7 +25,7 @@ public class ChangeZoneDifficulty : MonoBehaviour
 	private void DifficultyManager_DifficultyChanging(object sender, Difficulty difficulty)
 	{
 		if (difficulty == Difficulty.Easy)
-			generator.SetDifficulty(0);
+			generator.SetDifficulty(UnityEngine.Random.Range(0, 2));
 		else if (difficulty == Difficulty.Medium || difficulty == Difficulty.Hard)
 			generator.SetDifficulty(1);
 		else if (difficulty == Difficulty.VeryHard)
