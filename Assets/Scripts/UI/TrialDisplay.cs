b2ba71ee01f7901893a980682ac72ab0b947ef11
Zineddine Edoo <zineddine.edoo@outlook.com>
2019-11-11 16:17:48 +0800

    Added Trial Message Display

---
+++ Assets/Scripts/UI/TrialDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TrialDisplay : MonoBehaviour
+{
+	[SerializeField]
+	private RectTransform trialPanel = default;
+
+#if INSTANT
+	void Awake()
+	{
+		trialPanel.gameObject.SetActive(true);
+	}
+#endif
+}
