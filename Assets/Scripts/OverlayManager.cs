24b58e85cd68d27c068156afcedabaf1052c28c6
Zineddine Edoo <zineddine.edoo@outlook.com>
2019-11-16 19:31:34 +0800

    Added Overlay

---
+++ Assets/Scripts/OverlayManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class OverlayManager : MonoBehaviour
+{
+	public enum Result
+	{
+		OK,
+		Cancel
+	}
+
+	[SerializeField]
+	private TextMeshProUGUI messageText = default;
+
+	private Result? result;
+
+	// Need to prevent multiple calls
+	public IEnumerator ShowOverlay(string message, Action<Result> resultCallback)
+	{
+		result = null;
+		messageText.SetText(message);
+
+		GetComponentInChildren<Animator>().SetBool("FadeIn", true);
+
+		yield return new WaitUntil(() => result != null);
+
+		resultCallback?.Invoke((Result) result);
+	}
+
+	private void HideOverlay()
+	{
+		GetComponentInChildren<Animator>().SetBool("FadeIn", false);
+
+		//messageText.SetText("");
+	}
+
+	public void OnOkSelected()
+	{
+		result = Result.OK;
+		HideOverlay();
+	}
+
+	public void OnCancelSelected()
+	{
+		result = Result.Cancel;
+		HideOverlay();
+	}
+}
---
+++ Assets/Scripts/UI/Button/OverlayButton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using static OverlayManager;
+
+/// <summary>
+/// Requires OverlayManager Component in Parent Hierarchy
+/// </summary>
+public class OverlayButton : MonoBehaviour
+{
+	[SerializeField]
+	private Result buttonType = default;
+
+	void Awake()
+	{
+		GetComponent<Button>().onClick.AddListener(() =>
+		{
+			if (buttonType == Result.OK)
+				GetComponentInParent<OverlayManager>().OnOkSelected();
+			else
+				GetComponentInParent<OverlayManager>().OnCancelSelected();
+		});
+	}
+}
