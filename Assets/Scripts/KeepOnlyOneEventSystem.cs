2d62acfbfedc30cc2c473f1e826adcc438f1c7ec
Zineddine Edoo <zineddine.edoo@outlook.com>
2019-11-20 01:38:38 +0800

    Added Multiple Event System Check

---
+++ Assets/Scripts/KeepOnlyOneEventSystem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class KeepOnlyOneEventSystem : MonoBehaviour
+{
+	void Awake()
+	{
+		var eventSystems = FindObjectsOfType<EventSystem>();
+
+		if (eventSystems.FirstOrDefault(e => e.gameObject != gameObject))
+			Destroy(gameObject);
+	}
+}
