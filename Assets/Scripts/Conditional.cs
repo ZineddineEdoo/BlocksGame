4f9212cdb7da87048d1576a4eb95ef63441a2b52
Zineddine Edoo <zineddine.edoo@outlook.com>
2019-12-04 17:23:07 +0800

    Added Conditional for Activating / Deactivating Gameobjects based on Build Platform

---
+++ Assets/Scripts/Conditional.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Conditional : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject androidGameObject = default;
+
+	[SerializeField]
+	private GameObject uwpGameObject = default;
+
+	void OnValidate()
+	{
+#if UNITY_ANDROID
+		ActivateAndroid();
+#elif UNITY_WSA
+		ActivateUWP();
+#endif
+	}
+
+	private void ActivateAndroid()
+	{
+		androidGameObject.SetActive(true);
+		uwpGameObject.SetActive(false);
+	}
+
+	private void ActivateUWP()
+	{
+		androidGameObject.SetActive(false);
+		uwpGameObject.SetActive(true);
+	}
+}
